4d7079da70da7e194327d366f82b744e3960a604
Vooriden <emanuele.gullino@mail.com>
2018-03-18 22:26:00 +0100

    (As last commit)

--- Lab01/app/src/main/java/com/polito/ignurance/lab01/editActivity.java
+++ Lab01/app/src/main/java/com/polito/ignurance/lab01/editActivity.java
@@ -3,6 +3,7 @@ package com.polito.ignurance.lab01;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -14,33 +15,46 @@ import android.widget.Toast;
 
 public class editActivity extends AppCompatActivity {
 
+    //toolbar
+    Toolbar toolbar;
+
+    //edit texts
     EditText name;
     EditText mail;
     EditText bio;
 
-    CharSequence nameText;
-    CharSequence mailText;
-    CharSequence bioText;
+    //some kinds of variables
+    String nameText;
+    String mailText;
+    String bioText;
 
+    //shared preferences
     SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setTitle("Edit your profile");
+        preferences = getSharedPreferences("Info", Context.MODE_PRIVATE);
 
-        name = (EditText)findViewById(R.id.name);
-        mail = (EditText)findViewById(R.id.mail);
-        bio = (EditText)findViewById(R.id.bio);
+        setToolbar();
+        getEditTexts();
+        getTextsFromPreferences();
+        setTexts();
+    }
 
-        SharedPreferences prefs = getSharedPreferences("Info", Context.MODE_PRIVATE);
-        String textData = prefs.getString(TEXT_DATA_KEY, "No Preferences!");
-        TextView outputView = (TextView) findViewById(R.id.outputData);
-        outputView.setText(textData);
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        setTexts();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getTexts();
     }
 
     @Override
@@ -55,45 +69,77 @@ public class editActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_edit:
-                preferences = getSharedPreferences("Info", Context.MODE_PRIVATE);
-                Toast missing;
-
-                SharedPreferences.Editor editor = preferences.edit();
-                nameText = name.getText();
-                mailText = mail.getText();
-                bioText = bio.getText();
-
-                if (nameText != null) {
-                    editor.putString("Name", nameText.toString());
-                    editor.commit();
-                }
-                else{
-                    missing = Toast.makeText(this, "Insert your name", Toast.LENGTH_SHORT);
-                    missing.show();
-                    return true;
-                }
-
-                if (mailText != null) {
-                    editor.putString("Mail", mailText.toString());
-                    editor.commit();
-                }
-                else {
-                    missing = Toast.makeText(this, "Insert your mail", Toast.LENGTH_SHORT);
-                    missing.show();
-                    return true;
-                }
-
-                if (bioText != null) {
-                    editor.putString("Bio", bioText.toString());
-                    editor.commit();
-                }
-                else {
-                    editor.remove("Bio");
-                }
-                onBackPressed();
-                return true;
+                editor = preferences.edit();
+                getTexts();
+                return checkInput();
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //Set app Toolbar
+    private void setToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Edit your profile");
+    }
+
+    private boolean checkInput(){
+        Toast missing;
+
+        if (!nameText.isEmpty()) {
+            editor.putString("Name", nameText.toString());
+        }
+        else{
+            missing = Toast.makeText(this, "Insert your name", Toast.LENGTH_SHORT);
+            missing.show();
+            return false;
+        }
+
+        if (!mailText.isEmpty()) {
+            editor.putString("Mail", mailText.toString());
+        }
+        else {
+            missing = Toast.makeText(this, "Insert your mail", Toast.LENGTH_SHORT);
+            missing.show();
+            return false;
+        }
+
+        if (!bioText.isEmpty()) {
+            editor.putString("Bio", bioText.toString());
+        }
+        else {
+            editor.remove("Bio");
+        }
+
+        editor.commit();
+        finish();
+        return true;
+    }
+
+    private void getTexts(){
+        nameText = name.getText().toString();
+        mailText = mail.getText().toString();
+        bioText = bio.getText().toString();
+    }
+
+    private void getTextsFromPreferences(){
+        nameText = preferences.getString("Name", "Name");
+        mailText = preferences.getString("Mail", "Mail");
+        bioText = preferences.getString("Bio", "No Bio");
+    }
+
+    //Sel all the texts
+    private void setTexts(){
+        name.setText(nameText);
+        mail.setText(mailText);
+        bio.setText(bioText);
+    }
+
+    //Get all the text views
+    private void getEditTexts(){
+        name = (EditText) findViewById(R.id.name);
+        mail = (EditText)findViewById(R.id.mail);
+        bio = (EditText)findViewById(R.id.bio);
     }
 }
--- Lab01/app/src/main/java/com/polito/ignurance/lab01/showActivity.java
+++ Lab01/app/src/main/java/com/polito/ignurance/lab01/showActivity.java
@@ -1,28 +1,50 @@
 package com.polito.ignurance.lab01;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class showActivity extends AppCompatActivity {
 
+    //Toolbar
+    Toolbar toolbar;
+
+    //TextViews
     TextView name;
+    TextView mail;
+    TextView bio;
+
+    //SharedPreferences
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
-        name = (TextView)findViewById(R.id.name);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setTitle(name.getText());
+        preferences = getSharedPreferences("Info", Context.MODE_PRIVATE);
+
+        getTextViews();
+        setToolbar();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        setTexts();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setTexts();
     }
 
     @Override
@@ -37,11 +59,33 @@ public class showActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_edit:
+                //Edit user profile
                 Intent edit = new Intent(showActivity.this, editActivity.class);
                 startActivity(edit);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //Set app Toolbar
+    private void setToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(name.getText());
+    }
+
+    //Get all the text views
+    private void getTextViews(){
+        name = (TextView)findViewById(R.id.name);
+        mail = (TextView)findViewById(R.id.mail);
+        bio = (TextView)findViewById(R.id.bio);
+    }
+
+    //Sel all the texts
+    private void setTexts(){
+        name.setText(preferences.getString("Name", "Name"));
+        mail.setText(preferences.getString("Mail", "Mail"));
+        bio.setText(preferences.getString("Bio", "No Bio"));
     }
 }
